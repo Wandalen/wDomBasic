f5a4b73fb8f08c238b4c404942a481eebb457058
Wandalen <wandalen.me@gmail.com>
2019-04-18 08:49:45 +0300

    auto commit

--- out/debug/dwtools/abase/rbrowser/DomBase.js
+++ out/debug/dwtools/abase/rbrowser/DomBase.js
@@ -3,7 +3,8 @@
 'use strict';
 
 /**
-  @module Tools/base/DomBase - Collection of routines for a browser to operate DOM elements and its events. No matter whatever problem in a browser you are trying to solve several routines of DomBase would help you to solve it faster. It has something in common with Jquery, but no significant overlap. Use the module to get access to shortcuts for a browser.
+  Collection of routines for a browser to operate DOM elements and its events. No matter whatever problem in a browser you are trying to solve several routines of DomBase would help you to solve it faster. It has something in common with Jquery, but no significant overlap. Use the module to get access to shortcuts for a browser.
+  @module Tools/base/DomBase
 */
 
 /**
