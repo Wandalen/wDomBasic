9120ac89de828d31fdf22e6cde4fe693e203068d
wandalen <wandalen.me@gmail.com>
2021-04-06 17:16:29 +0300

    .

---
+++ proto/wtools/abase_dom/l7/Sound.js
@@ -0,0 +1,85 @@
+(function _Sound_js_() {
+
+'use strict';
+
+const _ = _global_.wTools;
+var $ = typeof jQuery !== 'undefined' ? jQuery : null;
+
+// --
+// routines
+// --
+
+function play( o )
+{
+  if( _.strIs( o ) )
+  o = { url : o }
+
+  _.assert( _.strIs( o.url ) );
+  _.assert( arguments.length === 1, 'Expects single argument' );
+
+  if( !_.SoundsByUrl[ o.url ] )
+  _.SoundsByUrl[ o.url ] = new buzz.sound( o.url );
+
+  var sound = _.SoundsByUrl[ o.url ];
+  var result = sound.play();
+
+}
+
+play.defaults =
+{
+  url : null,
+}
+
+//
+
+function playTry( o )
+{
+  if( _.strIs( o ) )
+  o = { url : o }
+
+  _.assert( arguments.length === 1, 'Expects single argument' );
+
+  try
+  {
+    return play( o );
+  }
+  catch( err )
+  {
+    console.warn( 'cant paly',o.url );
+  }
+
+}
+
+playTry.defaults =
+{
+}
+
+playTry.defaults.__proto__ = play.defaults;
+
+// --
+// prototype
+// --
+
+const Proto =
+{
+
+  play : play,
+  playTry : playTry,
+
+  SoundsByUrl : {},
+
+}
+
+if( _.sound )
+{
+  _.mapExtend( _.sound,Proto );
+}
+else
+{
+  _.sound = Proto;
+  _.sound.__proto__ = _;
+}
+
+const _ = _global_.wTools.sound;
+
+})();
